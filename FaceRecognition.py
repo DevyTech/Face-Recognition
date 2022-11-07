cb01fef25b196ec304b8af2c27a39ed249455850
DevyTech <bawotongdevaldy@gmail.com>
2022-11-08 00:30:16 +0800

    modified

--- FaceRecognition.py
+++ FaceRecognition.py
@@ -47,7 +47,7 @@ while True:
     imgS = cv2.cvtColor(imgS, cv2.COLOR_BGR2RGB)
 
     facesCurFrame = face_recognition.face_locations(imgS)
-    encodesCurFace = face_recognition.face_encodings(img, facesCurFrame)
+    encodesCurFace = face_recognition.face_encodings(imgS, facesCurFrame)
 
     for encodeFace,faceLoc in zip(encodesCurFace,facesCurFrame):
         matches = face_recognition.compare_faces(encodeListKnow,encodeFace)
@@ -58,13 +58,13 @@ while True:
         if matches[matchIndex]:
             name = className[matchIndex].upper()
             print(name)
-            # y1,x2,y2,x1 = faceLoc
-            # y1, x2, y2, x1 = y1*4,x2*4,y2*4,x1*4
-            # cv2.rectangle(img,(x1,y1),(x2,y2),(0,255,0),2)
-            # cv2.rectangle(img,(x1,y2-35),(x2,y2),(0,255,0),cv2.FILLED)
-            # cv2.puText(img,name,(x1+6,y2-6),cv2.FONT_HERSHEY_COMPLEX,1,(255,255,255),2)
+            y1,x2,y2,x1 = faceLoc
+            y1, x2, y2, x1 = y1*4,x2*4,y2*4,x1*4
+            cv2.rectangle(img,(x1,y1),(x2,y2),(0,255,0),2)
+            cv2.rectangle(img,(x1,y2-35),(x2,y2),(0,255,0),cv2.FILLED)
+            cv2.putText(img,name,(x1+6,y2-6),cv2.FONT_HERSHEY_COMPLEX,1,(255,255,255),2)
             # markFace(name)
 
 
-    # cv2.imshow('Webcam', img)
-    # cv2.waitKey(1)
+    cv2.imshow('Webcam', img)
+    cv2.waitKey(1)
